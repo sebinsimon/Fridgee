f8ae548660605ffd0bc170e4f381b1e48022de6f
Sebin Simon <sebinsimon70@gmail.com>
2023-03-20 21:48:34 +0000

    Added login and signup activity

--- app/src/main/java/com/example/fridgee/SplashActivity.java
+++ app/src/main/java/com/example/fridgee/SplashActivity.java
@@ -17,8 +17,9 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 finish();
+
             }
         }, 4000);
     }
